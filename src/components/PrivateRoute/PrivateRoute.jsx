c835170fa30eb71aabf8511a7e6d8ff6d15c6d32
OlegatorIT <podlesnyoleg93@gmail.com>
2023-08-16 02:49:09 +0200

    add PropTypes

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { selectAuthentificated } from 'redux/authSlice';
 
 const PrivateRoute = ({ children, redirectTo = '/' }) => {
   const authentificated = useSelector(selectAuthentificated);
 
   return authentificated ? children : <Navigate to={redirectTo} />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  redirectTo: PropTypes.string,
 };
 
 export default PrivateRoute;
